8a1046a9081fcc209ab4c3c488a3eec16c77cfa8
rogerbf <roger@bergfalk.nu>
2017-04-23 22:56:16 +0200

    sentences

--- source/index.js
+++ source/index.js
@@ -1,4 +1,27 @@
-const words = require(`./common-words`)
-const pickOne = require(`prob.js`).zipf(1, words.length)
+const commonWords = require(`./common-words`)
+const zipf = require(`prob.js`).zipf(1, commonWords.length)
 
-module.exports = count => Array(count).fill(``).map(() => words[pickOne()])
+const defaultConfig = {
+  sentenceLength: {
+    min: 7,
+    max: 26
+  }
+}
+
+const words = (count) =>
+  Array(count).fill(``).map(() => commonWords[zipf()])
+
+const sentences = ({ sentenceLength: { min, max } }, count) =>
+  Array(count)
+  .fill(``)
+  .map(() =>
+    words(Math.ceil(min - 1 + Math.random() * (max - (min - 1))))
+    .join(` `)
+    .concat(`.`)
+    .replace(/^./, first => first.toUpperCase())
+  )
+
+module.exports = Object.assign(
+  words,
+  { sentences: sentences.bind(null, defaultConfig) }
+)
