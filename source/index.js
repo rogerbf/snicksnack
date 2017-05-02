f3eae5780eee85bb0458a4c0dbc4406909ecfc87
rogerbf <roger@bergfalk.nu>
2017-05-02 21:55:53 +0200

    refactor

--- source/index.js
+++ source/index.js
@@ -1,26 +1,56 @@
-const commonWords = require(`./common-words`)
-const zipf = require(`prob.js`).zipf(1, commonWords.length)
-const range = (count = 0) => Array(count).fill(undefined)
+const probjs = require(`prob.js`)
 
-const defaultConfig = {
+const defaults = {
   sentenceLength: {
     min: 7,
     max: 26
-  }
+  },
+  wordList: require(`./common-words`)
 }
 
-const words = count => range(count).map(() => commonWords[zipf()])
+const zipf = state => Object.assign(
+  state,
+  { zipf: probjs.zipf(1, state.wordList.length) }
+)
 
-const sentences = ({ sentenceLength: { min, max } }, words, count) =>
-  range(count)
-  .map(() =>
-    words(Math.ceil(min - 1 + Math.random() * (max - (min - 1))))
-    .join(` `)
-    .concat(`.`)
-    .replace(/^./, first => first.toUpperCase())
+const range = state => Object.assign(
+  state,
+  { range: (count = 0) => Array(count).fill(undefined) }
+)
+
+const words = state => {
+  const { range, zipf, wordList } = state
+  return Object.assign(
+    state,
+    { words: count => range(count).map(() => wordList[zipf()]) }
   )
+}
+
+
+const sentences = state => {
+  const { range, words, sentenceLength: { min, max } } = state
+  return Object.assign(
+    state,
+    {
+      sentences: count => (
+        range(count)
+        .map(() =>
+          words(Math.ceil(min - 1 + Math.random() * (max - (min - 1))))
+          .join(` `)
+          .concat(`.`)
+          .replace(/^./, first => first.toUpperCase())
+        )
+      )
+    }
+  )
+}
+
+const factory = configuration =>
+  [ zipf, range, words, sentences ]
+  .reduce((snicksnack, f) => f(snicksnack), configuration)
 
 module.exports = Object.assign(
-  words,
-  { sentences: sentences.bind(null, defaultConfig, words) }
+  userConfiguration => factory(userConfiguration),
+  factory(defaults),
+  defaults
 )
