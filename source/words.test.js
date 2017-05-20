9e0ee6095b20f1b2fcccd168617bbbb3200aac07
rogerbf <roger@bergfalk.nu>
2017-05-20 23:20:43 +0200

    initial

--- source/words.test.js
+++ source/words.test.js
@@ -1,5 +1,23 @@
 const words = require(`./words`)
 
 describe(`words`, () => {
-  
+  it(`adds a words method to the state`, () => {
+    const state = words({})
+    expect(state).toHaveProperty(`words`)
+    expect(typeof (state.words)).toEqual(`function`)
+  })
+
+  it(`returns a word from a wordlist`, () => {
+    const range = jest.fn(() => [ undefined ])
+    const wordList = [ `a` ]
+    const random = jest.fn(() => 1)
+
+    const initialState = { range, wordList, random }
+    const state = words(initialState)
+
+    const actual = state.words(1)
+    const expected = [ `a` ]
+
+    expect(actual).toEqual(expected)
+  })
 })
